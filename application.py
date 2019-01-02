693dd1686f0beb48bd9064b2f3a96c7ac28e2d80
byben4ik <testinglearn7@gmail.com>
2019-01-02 14:40:12 +0300

    ФиКстурА

--- application.py
+++ application.py
@@ -56,5 +56,58 @@ class Application:
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
+
+    def created_contact(self, new_contact_group):
+        wd = self.wd
+        self.open_created_new_contact()
+        # Created new contact
+        # Firstname
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(new_contact_group.firstname)
+        # Middlename
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(new_contact_group.middlename)
+        # Lastname
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(new_contact_group.lastname)
+        # Address
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(new_contact_group.address)
+        # Home phone
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(new_contact_group.homephone)
+        # Mobile phone
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(new_contact_group.mobile)
+        # E-mail
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(new_contact_group.email)
+        # Date Birthday
+        # Day Birthday
+        wd.find_element_by_name("bday").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/select/option[11]").click()
+        # Month Birthday
+        wd.find_element_by_name("bmonth").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/select[2]/option[6]").click()
+        # Year Birthday
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(new_contact_group.byear)
+        # Submint new contact
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+
+
+    def open_created_new_contact(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+
     def destroy (self):
         self.wd.quit()
